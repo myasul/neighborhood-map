d7f48dc5a9ac59047b52b7f432fe0d784f3d8060
myasul <yasulmatthew@gmail.com>
2018-05-14 15:36:03 +0800

    Changes made:     - Add Knockout codebase     - Create a json file containing a list of the first 40 restaurants in BGC       using Zomato API     - Display all restaurants using Knockout

--- js/app.js
+++ js/app.js
@@ -1,9 +1,29 @@
 let map;
 
 function init_map() {
-    alert("hello!")
     map = new google.maps.Map($('#map').get(0), {
         center: { lat: 14.5408671, lng: 121.0503183 },
         zoom: 15,
     })
 }
+
+let Restaurant = function(data) {
+    this.restaurant_id = ko.observable(data.id);
+    this.lat = ko.observable(data.location.latitude);
+    this.lng = ko.observable(data.location.longitude);
+    this.name = ko.observable(data.name);
+}
+
+let ViewModel = function() {
+    let self = this;
+    this.restaurant_list = ko.observableArray();
+
+    $.getJSON("restaurants_zomato.json", function(json) {
+        $.each(json.restaurants, function() {
+            self.restaurant_list.push(new Restaurant(this.restaurant));
+        });
+    });
+
+}
+
+ko.applyBindings(new ViewModel());
