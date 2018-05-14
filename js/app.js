a12ddaf5d1606ba293b4c09c6b7ee33fb77df353
myasul <yasulmatthew@gmail.com>
2018-05-14 18:05:29 +0800

    Changes made:     - Add marker based on the places in the list.     - Add animation.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,5 @@
 let map;
-
-function init_map() {
-    map = new google.maps.Map($('#map').get(0), {
-        center: { lat: 14.5408671, lng: 121.0503183 },
-        zoom: 15,
-    })
-}
+let markers = [];
 
 let Restaurant = function(data) {
     this.restaurant_id = ko.observable(data.id);
@@ -15,6 +9,7 @@ let Restaurant = function(data) {
 }
 
 let ViewModel = function() {
+    console.log(new Date());
     let self = this;
     this.restaurant_list = ko.observableArray();
 
@@ -24,6 +19,61 @@ let ViewModel = function() {
         });
     });
 
+    if (!markers) {
+        init_markers(this.restaurant_list);
+    }
 }
 
-ko.applyBindings(new ViewModel());
+function init_map() {
+    console.log(new Date());
+    map = new google.maps.Map($('#map').get(0), {
+        center: { lat: 14.5408671, lng: 121.0503183 },
+        zoom: 15,
+    });
+    var bounds = new google.maps.LatLngBounds();
+    if (!view_model.restaurant_list().length == 0) {
+        bounds = init_markers(view_model.restaurant_list(), bounds);
+        map.fitBounds(bounds);
+        map.setZoom(16);
+    }
+}
+
+function init_markers(restaurant_list, bounds) {
+    $.each(restaurant_list, function() {
+        let location = {};
+        location.lat = parseFloat(this.lat());
+        location.lng = parseFloat(this.lng());
+        console.log(location);
+        var marker = new google.maps.Marker({
+            position: location,
+            animation: google.maps.Animation.DROP,
+            title: this.name(),
+            map: map,
+        });
+        let copy_marker;
+        marker.addListener('click', toggle_bounce);
+        markers.push(marker); // TODO: check if needed
+        bounds.extend(location);
+    });
+    console.log(bounds);
+    return bounds;
+}
+
+function toggle_bounce() {
+    var self = this;
+    if (!this.getAnimation()) {
+        this.setAnimation(google.maps.Animation.BOUNCE);
+        window.setTimeout(function() {
+            self.setAnimation(null);
+        }, 2000)
+    } else {
+        this.setAnimation(null);
+    }
+}
+
+function init_map_with_delay() {
+    window.setTimeout(init_map, 1000);
+}
+
+let view_model = new ViewModel();
+ko.applyBindings(view_model);
